8793aceb17f485180e252bbceb47b81934ece8f5
Tugdual de Kerviler <dekervit@gmail.com>
2020-06-22 18:13:58 +0200

    Use french patterns for encoder

--- src/encoder.py
+++ src/encoder.py
@@ -49,8 +49,7 @@ class Encoder:
         self.bpe_ranks = dict(zip(bpe_merges, range(len(bpe_merges))))
         self.cache = {}
 
-        # Should haved added re.IGNORECASE so BPE merges can happen for capitalized versions of contractions
-        self.pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+        self.pat = re.compile(r""" ?j’| ?t’| ?s’| ?c’| ?m’| ?n’| ?l’| ?d’| ?qu’| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""", re.UNICODE | re.IGNORECASE)
 
     def bpe(self, token):
         if token in self.cache:
